d05b595f9a19b9cbe6426f253275c58dd2a687d8
Nikesh Sitaula <sitaulanike@gmail.com>
2024-09-26 15:57:40 -0400

    add tv calculations

--- src/main/java/com/pluralsight/Demo1.java
+++ src/main/java/com/pluralsight/Demo1.java
@@ -1,8 +1,33 @@
 package com.pluralsight;
 
+import java.awt.desktop.SystemEventListener;
+
 public class Demo1 {
 
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        char status = 'm';
+        int identifier = 1;
+        String name = "Nikesh";
+
+        String greeting = "Hello " + name;
+        //String greeting = "Hello" + "Nikesh";
+        //String greeting = "Hello Nikesh";
+
+        int numberOfDaysPerWeekWatchesTV = 5;
+        int minutesPerDayOnDayWatchedOnAverage = 70;
+
+        int minutesPerWeek = numberOfDaysPerWeekWatchesTV * minutesPerDayOnDayWatchedOnAverage;
+        int minutesPerYear = minutesPerWeek * 52;
+
+        int hoursPerYear = minutesPerYear / 60;
+
+        int hoursInWorkWeek = 35;
+
+        int workWeeksPerYearWatchingTV = hoursPerYear / hoursInWorkWeek;
+
+        System.out.println("If " + name + " did not watch TV, it would be like a " + workWeeksPerYearWatchingTV + " week vacation every year");
+        System.out.println(greeting);
     }
 }
